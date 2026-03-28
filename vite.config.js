24648be5e0fd9c3d711f548690e2f37056f5dc95
Rohan Mia <rohanmia.org@gmail.com>
2026-03-29 04:48:56 +0600

    updated base name for github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-wrold-on-the-goo/",
+  base: "/react-world-on-the-goo/",
 });
